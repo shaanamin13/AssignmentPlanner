b2d436a37d7281c47c70c551f0f0429e4150934a
shaanamin13 <shaan-amin@uiowa.edu>
2014-11-09 19:14:54 -0600

    Add Assignments Activity

---
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.shaan.assignmentplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Shaan on 11/9/2014.
+ */
+public class HomeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/NewAssignmentsActivity.java
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/NewAssignmentsActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class AssignmentsActivity extends Activity {
+public class NewAssignmentsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
