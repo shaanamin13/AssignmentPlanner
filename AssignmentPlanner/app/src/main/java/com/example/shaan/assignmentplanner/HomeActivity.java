3072195bd7fc4d0feff102aa76ebfbb84c4c718c
shaanamin13 <shaan-amin@uiowa.edu>
2014-11-09 19:38:06 -0600

    Added button to main

--- AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
@@ -1,15 +1,30 @@
 package com.example.shaan.assignmentplanner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Shaan on 11/9/2014.
  */
 public class HomeActivity extends Activity {
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void addAssignment(View view){
+        Intent intent = new Intent(HomeActivity.this, NewAssignmentsActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
