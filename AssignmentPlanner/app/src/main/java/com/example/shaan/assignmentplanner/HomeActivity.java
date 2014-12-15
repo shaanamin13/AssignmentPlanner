bcea1d91e0b6066894a61619f7459cc041a1ca88
shaanamin13 <shaan-amin@uiowa.edu>
2014-12-15 16:55:19 -0600

    THis is awesome

--- AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
@@ -1,16 +1,12 @@
 package com.example.shaan.assignmentplanner;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-/**
- * Created by Shaan on 11/9/2014.
- */
-public class HomeActivity extends Activity {
+
+public class HomeActivity extends FragmentActivity {
 
 
 
