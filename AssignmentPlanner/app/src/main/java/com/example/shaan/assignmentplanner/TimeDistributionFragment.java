68155c40c3c69613807ba4d318306ced33528408
shaanamin13 <shaan-amin@uiowa.edu>
2014-12-16 12:44:56 -0600

    THis is awesome

---
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/TimeDistributionFragment.java
@@ -0,0 +1,14 @@
+package com.example.shaan.assignmentplanner;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TimeDistributionFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_time_distribution, container, false);
+    }
+}
