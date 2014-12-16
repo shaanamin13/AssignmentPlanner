edb470a0293c2aa4a2bfe51f5a2d44470d4ad1f3
shaanamin13 <shaan-amin@uiowa.edu>
2014-12-16 15:36:29 -0600

    Everything committed

--- AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/HomeActivity.java
@@ -1,26 +1,55 @@
 package com.example.shaan.assignmentplanner;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 
 public class HomeActivity extends FragmentActivity {
 
+    ViewPager viewPager = null;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new MyAdapter(fragmentManager));
     }
-
-    public void addAssignment(View view){
-        Intent intent = new Intent(HomeActivity.this, NewAssignmentsActivity.class);
-        startActivity(intent);
-
-    }
-
 
 }
+
+class MyAdapter extends FragmentPagerAdapter {
+
+
+    public MyAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = null;
+
+        if (i == 0) {
+            fragment = new AddFragment();
+        }
+        if (i == 1) {
+            fragment = new CalendarFragment();
+        }
+        if (i == 2) {
+            fragment = new TimeDistributionFragment();
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
+
--- AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/TimeDistributionFragment.java
+++ AssignmentPlanner/app/src/main/java/com/example/shaan/assignmentplanner/TimeDistributionFragment.java
@@ -1,11 +1,10 @@
 package com.example.shaan.assignmentplanner;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.support.v4.app.Fragment;
 
 public class TimeDistributionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
